a4257258c02180f6fdcb3d153025cfa6252af620
zhopkinsdev <zhopkinsdev@gmail.com>
2019-06-17 23:39:20 +0000

    revising migrations

--- db/migrate/XXXXX_create_places.rb
+++ db/migrate/XXXXX_create_places.rb
@@ -1,7 +1,7 @@
 class CreatePlaces < ActiveRecord::Migration[5.2]
   def change
     create_table :places do |t|
-       t.string :name
+      t.string :name
       t.text :description
       t.string :address
       t.timestamps
