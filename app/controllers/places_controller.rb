c41399a9f9c4e6b028e036d223f449c4edbc57b2
zhopkinsdev <zhopkinsdev@gmail.com>
2019-06-25 01:03:40 +0000

    revised place_controller file

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,21 +9,13 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
-  
-@place.update_attributes(place_params)
-  if @place.valid?
-    redirect_to root_path
-  else
-    render :edit, status: :unprocessable_entity
-  end
-end
-
-
   def create
-    Place.create(place_params)
-    redirect_to root_path
-  end
-
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -32,33 +24,31 @@ end
   end
 
   def edit
-  @place = Place.find(params[:id])
-
-  if @place.user != current_user
-    return render plain: 'Not Allowed', status: :forbidden
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
-end
 
   def update
     @place = Place.find(params[:id])
-
     if @place.user != current_user
-    return render plain: 'Not Allowed', status: :forbidden
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
 
   def destroy
-  @place = Place.find(params[:id])
-  if @place.user != current_user
-    return render plain: 'Not Allowed', status: :forbidden
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+    @place.destroy
+    redirect_to root_path
   end
-
-  @place.destroy
-  redirect_to root_path
-end
 
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-
+end
