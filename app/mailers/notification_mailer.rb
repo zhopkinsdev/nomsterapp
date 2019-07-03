4689bcf66bff696c6a04685e043b5d187e21c32e
zhopkinsdev <zhopkinsdev@gmail.com>
2019-07-03 04:52:56 +0000

    39 finally works, email error needs fixing

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -6,7 +6,7 @@ class NotificationMailer < ApplicationMailer
   def comment_added(comment)
     @place = comment.place
     @place_owner = @place.user
-    mail(to: @place_rayazand@gmail.com,
+    mail(to: @rayazand@gmail.com,
          subject: "A comment has been added to #{@place.name}")
   end
 end
