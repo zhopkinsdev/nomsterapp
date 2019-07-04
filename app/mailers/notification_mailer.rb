d0dc0d7a43cbc33753dee04e389534a7777a64d5
zhopkinsdev <zhopkinsdev@gmail.com>
2019-07-04 03:53:40 +0000

    be sure to turn on the less secure apps switch on google to get things working link is here: https://myaccount.google.com/lesssecureapps

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,11 +1,12 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  
+
   def comment_added(comment)
     @place = comment.place
     @place_owner = @place.user
-    mail(to: rayazand@gmail.com,
+    delivery_options = {user_name: ENV["GMAIL_ADDRESS"], password: ENV["GMAIL_PASSWORD"] }
+    mail(to: "rayazand+emailtest@gmail.com",
          subject: "A comment has been added to #{@place.name}")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
+  config.action_mailer.smtp_settings = { host:'localhost', port: '3000' }
     address: "smtp.gmail.com",
     port: 587,
     domain: "example.com",
