0c1f4dfeb8cd4f383cf39025e1b3b609cffccf6f
zhopkinsdev <zhopkinsdev@gmail.com>
2019-07-04 02:31:32 +0000

    emailer gon bad,ruined server

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,12 +1,11 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
- 
-
+  
   def comment_added(comment)
     @place = comment.place
     @place_owner = @place.user
-    mail(to: @rayazand@gmail.com,
+    mail(to: rayazand@gmail.com,
          subject: "A comment has been added to #{@place.name}")
   end
 end
