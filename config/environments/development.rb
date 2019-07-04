504014a75ab8569fb769f355666cdf7d7de0e25c
zhopkinsdev <zhopkinsdev@gmail.com>
2019-07-04 04:02:24 +0000

    fixing email functions on comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,17 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
 
-  config.action_mailer.smtp_settings = { host:'localhost', port: '3000' }
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+
+  #config.action_mailer.smtp_settings = {
+    #address: "smtp.gmail.com",
+   # port: 587,
+   # domain: "example.com",
+    #authentication: "plain",
+    #enable_starttls_auto: true,
+   # user_name: ENV['GMAIL_ADDRESS'],
+   # password: ENV['GMAIL_PASSWORD']
+    #}
 
   config.action_mailer.default_url_options = { host: 'https://nomster-raya-anderson.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
