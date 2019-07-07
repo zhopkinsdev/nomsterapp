c15774187f353f2da943649ee8c358adb08633a8
zhopkinsdev <zhopkinsdev@gmail.com>
2019-07-07 21:45:37 +0000

    forgotten files

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user! only: 
 
    def create
     @place = Place.find(params[:place_id])
