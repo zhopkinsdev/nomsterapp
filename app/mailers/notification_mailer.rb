a532e03bde1d0e2f2d99fc41a58600c6cef73ada
zhopkinsdev <zhopkinsdev@gmail.com>
2019-07-22 21:31:07 +0000

    removed line 8

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -5,7 +5,6 @@ class NotificationMailer < ApplicationMailer
   def comment_added(comment)
     @place = comment.place
     @place_owner = @place.user
-    delivery_options = {user_name: ENV["GMAIL_ADDRESS"], password: ENV["GMAIL_PASSWORD"] }
-     mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name}")
+    mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name}")
   end
 end
