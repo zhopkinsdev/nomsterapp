cacacb7e3bafce1c47a5ddbdea9704d04216af2d
zhopkinsdev <zhopkinsdev@gmail.com>
2019-07-22 21:25:24 +0000

    fixing comment section

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -6,7 +6,6 @@ class NotificationMailer < ApplicationMailer
     @place = comment.place
     @place_owner = @place.user
     delivery_options = {user_name: ENV["GMAIL_ADDRESS"], password: ENV["GMAIL_PASSWORD"] }
-    mail(to: "rayazand+emailtest@gmail.com",
-         subject: "A comment has been added to #{@place.name}")
+     mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name}")
   end
 end
