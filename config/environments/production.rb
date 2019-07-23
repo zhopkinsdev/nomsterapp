3a877d6c6969fe7961540b09e9b65cca85800776
zhopkinsdev <zhopkinsdev@gmail.com>
2019-07-23 02:56:35 +0000

    fising comments again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
+  config.action_mailer.default_url_options = { host: 'nomster-raya-anderson.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
